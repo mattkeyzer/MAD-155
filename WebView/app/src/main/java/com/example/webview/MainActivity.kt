96a3152566c0018ba55285acf290a44aab1ca2b7
mattkeyzer <105663113+mattkeyzer@users.noreply.github.com>
2023-09-18 16:42:40 -0500

    webview commit

---
+++ WebView/app/src/main/java/com/example/webview/MainActivity.kt
@@ -0,0 +1,21 @@
+package com.example.webview
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.webkit.WebView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val myWeb = findViewById<WebView>(R.id.wb_webView)
+
+        myWeb.apply {
+            loadUrl("https://www.google.com")
+            settings.javaScriptEnabled = true
+        }
+
+
+    }
+}
